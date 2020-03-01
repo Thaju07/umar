bd4442e394db3e84f1973cfc06c78a86e3fc42af
Thaju07 <you@example.com>
2020-03-01 17:56:43 +0530

    umar

---
+++ intex.js
@@ -0,0 +1,14 @@
+for(i=0;i<=8;i++)
+{
+    for(j=0;j<=8;j++)
+    {
+      for(k=0;k<=8;k++)
+      {
+          console.log(j,k)
+          for(l=0;l<=8;l++)
+          {
+              console.log(i,l)
+          }
+      }
+    }
+}
